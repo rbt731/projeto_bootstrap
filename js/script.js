09ffd85e1db96a61f624f2f22b98142a77cc01f0
robert <robertotaku233@gmail.com>
2023-08-10 20:31:19 -0300

    Fienalizado formulario com java script

--- js/script.js
+++ js/script.js
@@ -31,15 +31,17 @@ idade.addEventListener("blur", function(){
 })
 
 let endereco = document.querySelector("#endereco")
-
 endereco.addEventListener("blur", ()=>{
-    if(endereco.value.length >= 35){
-    alert("O endereço esta muito grande!")
-        endereco.style.border = "2px solid red"
-    }
-    else{
-        alert("O endereço está OK!")
-        endereco.style.border = "2px solid yellow"
+    if(endereco.value != ""){
+        if(endereco.value.length >= 35){
+        alert("O endereço esta muito grande!")
+            endereco.style.border = "2px solid red"
+        }
+        else{
+            alert("O endereço está OK!")
+            endereco.style.border = "2px solid yellow"
+        }
+
     }
 })
 
@@ -159,4 +161,31 @@ trabalho.addEventListener("change", ()=>{
             }
         }
     }
+})
+
+let cor = document.querySelector("#cor")
+cor.addEventListener("change", ()=>{
+    console.log(cor.value)
+    document.body.style.backgroundColor = cor.value
+})
+
+let mensagem = document.querySelector("#mensagem")
+let restante =  document.querySelector("#restante")
+let limite = 20
+mensagem.addEventListener("keyup", ()=>{
+    // console.log(mensagem.value.length)
+    restante.textContent = mensagem.value.length
+
+    console.log(restante.parentNode)
+
+    if(restante.textContent == 20){
+        mensagem.classList.add("border-danger")
+        restante.parentNode.style.color = "red"
+    }
+    else{
+        mensagem.classList.remove("border-danger")
+        restante.parentNode.style.color = "black"
+    }
+
+    mensagem.setAttribute("maxlength", limite)
 })
